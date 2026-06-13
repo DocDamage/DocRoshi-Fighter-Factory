42727668d21a1841201ee439c8d5c7c960b6036b
DocDamage <dsferraro@gmail.com>
2026-06-13 18:41:38 -0400

    Extract core tab builders

--- tests/test_smoke_v7_5.py
+++ tests/test_smoke_v7_5.py
@@ -57,6 +57,7 @@ class MugenForgeSmokeTests(unittest.TestCase):
             'mugenforge.ui_tabs.binary',
             'mugenforge.ui_tabs.closure_evidence',
             'mugenforge.ui_tabs.continuity',
+            'mugenforge.ui_tabs.core_builders',
             'mugenforge.ui_tabs.creator_factory_tabs',
             'mugenforge.ui_tabs.creator_hub',
             'mugenforge.ui_tabs.forge_workspaces',
